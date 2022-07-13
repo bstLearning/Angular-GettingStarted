8d6b0403ec55b01d8bb57199519061eb19f0096c
Benson <48788513+benson00077@users.noreply.github.com>
2022-07-13 18:01:05 +0800

    Learn ch7
    More on Components
    Defining interfaace
    Add component css
    Using lifecycle hooks
    Building custom pipes
    Getters and Setters

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,17 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Iproduct } from "./prouduct";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export default class ProductListComponent {
+export default class ProductListComponent implements OnInit{
     pageTitle = 'Product List!';
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
-    listFilter = 'cart';
-    products = [
-        {
+
+    private _listFilter: string = '';
+    get listFilter(): string {
+      return this._listFilter
+    }
+    set listFilter(value: string) {
+      this._listFilter = value
+      console.log('In setter: ', value);
+      this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: Iproduct[] = []
+    products: Iproduct[] = [
+        { 
           "productId": 1,
           "productName": "Leaf Rake",
           "productCode": "GDN-0011",
@@ -33,7 +46,17 @@ export default class ProductListComponent {
         },
     ];
 
+    performFilter(filterBy: string): Iproduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: Iproduct) => 
+       product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
     toggleImage(): void {
       this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+      this.listFilter = 'cart';
     }
 };
