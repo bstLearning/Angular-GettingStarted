beec71b9fd650bbbad3a38086fb578ed13736854
Benson <48788513+benson00077@users.noreply.github.com>
2022-08-18 17:53:54 +0800

    Learn ch11
    Navigation and Routing basics
    To nest or to route

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import ProductListComponent from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/stat.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,20 @@ import { StarComponent } from './shared/stat.component';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [AppComponent]
 })
